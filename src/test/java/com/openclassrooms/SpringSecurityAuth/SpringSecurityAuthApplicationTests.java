1b074e5a3f8efc2f4d8c42ece2c240eb115a3b6c
Samy-Bodio2 <samybodio8@gmail.com>
2023-11-24 13:17:41 +0100

    first commit

---
+++ src/test/java/com/openclassrooms/SpringSecurityAuth/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.SpringSecurityAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
